6a4faf84d8fb791d64be056a10a501efc497fd23
eedu7 <mueedahmad067@gmail.com>
2025-01-07 09:54:47 +0500

    FEAT: Added the middleware of the sqlalchemy to the server and also added the CORSMiddleware

--- backend/core/server.py
+++ backend/core/server.py
@@ -1,16 +1,36 @@
+
+from typing import List
+
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import router
+from core.fastapi.middlewares import (SQLAlchemyMiddleware)
 
 
 def init_routers(app_: FastAPI) -> None:
     app_.include_router(router)
 
 
+def make_middleware() -> List[Middleware]:
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        ),
+        Middleware(SQLAlchemyMiddleware),
+    ]
+    return middleware
+
+
 def create_app() -> FastAPI:
     app_ = FastAPI(
-        title="E-Commerce User-Service",
-        description="Backend for the user service i.e Login, Register etc",
+        title="E-Commerce",
+        description="Backend for the E-Commerce API",
         version="1.0.1",
     )
     init_routers(app_)
