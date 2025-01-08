71091949b3bcaaa53eca6b5fcdb30ebdde40daad
eedu7 <mueedahmad067@gmail.com>
2025-01-08 09:27:33 +0500

    FEAT: Added the methods of fetching user by its email and its username in the user repository

--- backend/app/repositories/user.py
+++ backend/app/repositories/user.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Select
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import joinedload
 
 from app.models import User
+from core.exceptions import NotFoundException, BadRequestException
 from core.repository import BaseRepository
 
 
@@ -10,4 +12,8 @@ class UserRepository(BaseRepository[User]):
     User repository provides all the database operations for the User model.
     """
 
-    pass
+    async def get_by_email(self, email: str) -> User:
+        return await self.get_by(field="email", value=email)
+
+    async def get_by_username(self, username: str) -> User:
+        return await self.get_by(field="username", value=username)
