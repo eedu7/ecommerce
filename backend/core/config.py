f7ae441d78fc738b2b3cbc0ef43911979280f932
eedu7 <mueedahmad067@gmail.com>
2025-01-08 10:28:02 +0500

    FEAT: Added the JWT token encode and decode methods and renamed the JWT_EXPIRATION to JWT_EXPIRE_MINUTES

--- backend/core/config.py
+++ backend/core/config.py
@@ -15,7 +15,7 @@ class Config(BaseConfig):
     TEST_POSTGRES_URL: str
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
-    JWT_EXPIRATION: int = 60 * 60 * 7
+    JWT_EXPIRE_MINUTES: int = 60 * 60 * 7
 
 
 config: Config = Config()
