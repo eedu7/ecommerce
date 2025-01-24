9e0b1c72a9a1d2d38530988aab4b5ea964915197
eedu7 <mueedahmad067@gmail.com>
2025-01-24 17:21:24 +0500

    MINOR: Added the loginFormScheme, LoginFormScheme, registerFormScheme and RegisterFormScheme into a separate file

---
+++ frontend/src/features/authentication/scheme.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export const registerFormScheme = z
+    .object({
+        username: z
+            .string()
+            .min(3, { message: "Username must be at least 3 characters." })
+            .max(32, { message: "Username must be at most 32 characters." }),
+        email: z.string().email({ message: "Invalid email address." }),
+        password: z
+            .string()
+            .min(8, { message: "Password must be at least 8 characters." })
+            .max(16, {
+                message: "Password must be at most 16 characters long.",
+            }),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
+
+export const loginFormScheme = z.object({
+    username: z
+        .string()
+        .min(3, { message: "Username must be at least 3 characters." })
+        .max(32, { message: "Username must be at most 32 characters." }),
+    email: z.string().email({ message: "Invalid email address." }),
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters." })
+        .max(16, {
+            message: "Password must be at most 16 characters long.",
+        }),
+});
+
+export type RegisterFormSchema = z.infer<typeof registerFormScheme>;
+export type LoginFormSchema = z.infer<typeof loginFormScheme>;
