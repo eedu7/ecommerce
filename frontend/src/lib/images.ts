58cc4bf6e7442ff05bb2f4421db9b9584d9a25b7
eedu7 <mueedahmad067@gmail.com>
2025-01-26 20:17:13 +0500

    MINOR: Added the Logo

--- frontend/src/lib/images.ts
+++ frontend/src/lib/images.ts
@@ -1,3 +1,7 @@
 import ShoppingBags from "../../public/assets/images/shopping_bags.svg";
 
+import WebsiteLogo from "../../public/assets/icon/logo.svg";
+
 export default ShoppingBags;
+
+export { WebsiteLogo };
